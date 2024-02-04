dc39baaca617ad661c990ce35940696ba555fae0
sowro <sowrovnil59@gmail.com>
2024-02-05 02:30:05 +0600

    Bdoll

--- app/src/main/java/com/example/hostelbd1/MainActivity.java
+++ app/src/main/java/com/example/hostelbd1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hostelbd1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
--- app/src/main/java/com/example/hostelbd1/MainActivity2.java
+++ app/src/main/java/com/example/hostelbd1/MainActivity2.java
@@ -2,91 +2,78 @@ package com.example.hostelbd1;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.app.WindowDecorActionBar;
+import androidx.appcompat.view.ActionBarPolicy;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    ImageSlider imageSlider;
+    BottomNavigationView bottomNavagation;
 
-    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2); imageSlider =findViewById(R.id.image_slider);
-        recyclerView=findViewById(R.id.recyclerView);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.activity_main2);
 
-        imageslide();
-
-
-
-
-        Myadapter myadapter=new Myadapter();
-        recyclerView.setAdapter(myadapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity2.this,RecyclerView.VERTICAL,false));
+        bottomNavagation=findViewById(R.id.bottomNavagation);
 
 
 
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.framlayout,new BlankFragment());
+        fragmentTransaction.commit();
 
 
 
-
-
-
-
-    }
+        bottomNavagation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
 
 
 
-
-    private class Myadapter extends RecyclerView.Adapter<Myadapter.MyVideHolder>{
-
-
-        private class MyVideHolder extends RecyclerView.ViewHolder{
-
-
-            public MyVideHolder(@NonNull View itemView) {
-                super(itemView);
+                return true;
             }
-        }
+        });
 
-
-
-
-        @NonNull
-        @Override
-        public MyVideHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-
-            LayoutInflater layoutInflater=getLayoutInflater();
-
-            View myView=   layoutInflater.inflate(R.layout.layout,parent,false);
-
-            return new MyVideHolder(myView);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull MyVideHolder holder, int position) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return 10;
-        }
 
 
 
@@ -97,56 +84,6 @@ public class MainActivity2 extends AppCompatActivity {
 
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private void imageslide (){
-        ArrayList<SlideModel> imageList = new ArrayList<>();
-        imageList.add(new SlideModel(R.drawable.img, ScaleTypes.CENTER_CROP));
-        imageList.add(new SlideModel(R.drawable.img_1,ScaleTypes.CENTER_CROP));
-        imageList.add(new SlideModel(R.drawable.img_2,ScaleTypes.CENTER_CROP));
-        imageList.add(new SlideModel(R.drawable.sss,ScaleTypes.CENTER_CROP));
-        imageList.add(new SlideModel(R.drawable.img_4,ScaleTypes.CENTER_CROP));
-        imageSlider.setImageList(imageList);
-
-
-
-
-
-    }
-
-
-
-
 
 
 
